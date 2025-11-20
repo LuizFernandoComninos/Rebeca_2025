3586b40918f067b78552c33130cde146b7a582a1
LuizFernandoComninos <luizcomninos@gmail.com>
2025-11-20 19:53:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
+st.set_page_config(page_title="DashCovid", layou="wide")
+
 df = pd.read_csv('WHO_time_series.csv')
 
 df['Date_reported'] = pd.to_datetime(df['Date_reported'])
